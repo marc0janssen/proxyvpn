30cc24b62b590563ab2c18460e6f7e163aa7f37c
Marco <marco@mjanssen.nl>
2025-01-19 22:05:24 +0100

    proxyvpn.conf

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@
 # date: 2021-11-28 14:24:26
 # update: 2021-11-28 14:24:32
 
-VERSION="24.5"
-
 docker image rm marc0janssen/proxyvpn:stable
 
 docker buildx build --no-cache --platform linux/amd64,linux/arm64 --push -t marc0janssen/proxyvpn:stable -f ./Dockerfile .
